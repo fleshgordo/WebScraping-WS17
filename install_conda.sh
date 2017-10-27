621a1d92155cc1fd391d00d82a3f2700a9c12d75
Gordan Savicic <gordo@yugo.at>
2017-10-27 14:28:28 +0200

    first commit

---
+++ install_conda.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [[ $# -eq 0 ]] ; then
+   NAME="blockwoche"
+else
+  NAME=$1
+fi
+
+# remove anaconda environment
+conda remove -y --name $NAME --all
+
+# change to anaconda3 folder
+cd ~/anaconda3/bin/
+
+# install stuff
+./conda create -y -n $NAME python=2.7
+./conda install -y -n $NAME jupyter
+./conda install -y -n $NAME ipython
+./conda install -y -n $NAME -c conda-forge markovify
+./conda install -y -n $NAME -c conda-forge selenium
+./conda install -y -n $NAME -c conda-forge tweepy
+./conda install -y -n $NAME beautifulsoup4
+./conda install -y -n $NAME -c conda-forge googlemaps
+./conda install -y -n $NAME -c anaconda pycairo
+./conda install -y -n $NAME -c anaconda cairo
+
+echo 'export PATH=$HOME/anaconda3/bin:$PATH' >> ~/.bash_profile
